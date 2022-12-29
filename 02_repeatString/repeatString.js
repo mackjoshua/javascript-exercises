5b76124bc2f303d4f5743d2404baed9eb49dd768
Joshua <joshmack02@gmail.com>
2022-12-29 12:44:58 -0800

    completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(userString, quantity) {
+    let output = '';
 
+    if (quantity <= -1) {
+        return 'ERROR';
+    }
+
+    for (i = 0; i < quantity; i++) {
+        output += userString;
+    }
+    return output;
 };
 
 // Do not edit below this line
