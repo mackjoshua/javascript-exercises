c963ec5282527d9abd76302f089cb4af36597436
Joshua <joshmack02@gmail.com>
2022-12-29 13:20:08 -0800

    exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    let string = userString;
+    let splitString = string.split('');
+    splitString.reverse();
+    let finalString = splitString.join('');
+    return finalString;
 };
 
 // Do not edit below this line
