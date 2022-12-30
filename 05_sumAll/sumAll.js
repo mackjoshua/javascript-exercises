9e8aad9ad54632214006a77e9af50d9905f3673b
Joshua <joshmack02@gmail.com>
2022-12-29 18:05:45 -0800

    exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    let minNum;
+    let maxNum;
+//   Error handling for negative numbers
+    if ((num1 || num2) <= -1) {
+      return 'ERROR';
+    }
+//   Error handling for when a non-number parameter is provided
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      return 'ERROR';
+    }
+// This if statement assigns which number is the min or max, 
+// So that parameters can be provided in any order
+    if (num1 < num2) {
+      minNum = num1;
+      maxNum = num2;
+    } else {
+      minNum = num2;
+      maxNum = num1;
+    }
+  
+    for (i = minNum; i <= maxNum; i++) {
+      finalSum += i;
+    }
+    console.log(finalSum);
+    return finalSum;
+  }
 // Do not edit below this line
 module.exports = sumAll;
