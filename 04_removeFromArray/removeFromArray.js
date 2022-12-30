98e07b94d54d5d2510265b617b02d8ffc2fbf07b
Joshua <joshmack02@gmail.com>
2022-12-29 16:14:34 -0800

    completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(ourArray, ...moreArgs) {
+   let arrayLength = ourArray.length;
+//    The first for loop iterates over the array created by ...moreArgs
+   for (const value of moreArgs) {
+    // The next for loop then iterates over ourArray
+        for (i=0; i < arrayLength;i++) {
+            // This if statement then checks, for each value of moreArgs, if there is a matching value in ourArray by iterating over ourArray
+            if (ourArray[i] === value) {
+                ourArray.splice(i,1)
+            }
+        }    
+   }
+   return ourArray;
 };
+
+// So in sum, it grabs a value from moreArgs, 
+// then iterates over ourArray to see if there are any matching values
 
 // Do not edit below this line
 module.exports = removeFromArray;
