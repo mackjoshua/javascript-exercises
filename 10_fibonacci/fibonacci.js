785976757c33fcbf116b57c1c9f39e4725a31ce2
Joshua <joshmack02@gmail.com>
2023-01-03 13:59:19 -0800

    exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num, ourArray = [0,1,1]) {
+    num = Number(num);
+    if (num < 0) {
+    return 'OOPS';
+    }
+    for (i = 2; i <= num; i++) {
+        ourArray[i] = ourArray[i-2] + ourArray[i-1];
+    }
+    console.log(ourArray[num]);
+    return ourArray[num];
 };
 
 // Do not edit below this line
