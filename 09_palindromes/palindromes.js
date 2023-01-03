7e0fb9231b573fc60a2580fb871869c76c5f7a6a
Joshua <joshmack02@gmail.com>
2023-01-02 21:40:19 -0800

    exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"");
+    let reversedString = string.split('').reverse().join('');
+    return reversedString.toLowerCase() === string.toLowerCase();
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
