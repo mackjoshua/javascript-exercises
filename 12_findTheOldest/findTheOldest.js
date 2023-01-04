61569fa0a86974fc31630d9399ee33e967062904
Joshua <joshmack02@gmail.com>
2023-01-03 18:49:38 -0800

    exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+	let year = new Date();
+    	year = year.getFullYear();
+     
+  obj.sort(function(a, b) {
+    
 
+    a.age = a.yearOfDeath - a.yearOfBirth;
+    b.age = b.yearOfDeath - b.yearOfBirth;
+    
+       	if (a.yearOfDeath === undefined) {
+    	a.age = year - a.yearOfBirth;
+      /* return a.age; */
+    }
+    
+    if (b.yearOfDeath === undefined) {
+      b.age = year - b.yearOfBirth;
+      /* return b.age; */
+    }
+    
+    
+    if (a.age > b.age) {
+        return -1;
+    } else {
+        return 1;
+    }
+   })
+	console.log(obj);
+   return obj[0];
 };
 
 // Do not edit below this line
